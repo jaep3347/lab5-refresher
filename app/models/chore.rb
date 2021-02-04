6068513d0419d8ffcd95488e57b45a79b028d4d3
JP Park <jaepyunp@andrew.cmu.edu>
2021-02-04 17:09:36 -0500

    Models finished
    Models finished

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,18 @@
 class Chore < ApplicationRecord
-    belongs_to :child
-    belongs_to :task
+  belongs_to :child
+  belongs_to :task
+
+  validates_datetime :due_on
+
+  scope :by_task, -> {joins(:task).order('name') }
+  scope :chronological, -> { order('due_on', 'completed') }
+  scope :pending, -> {where(completed: false)}
+  scope :done, -> {where.not(completed: false)}
+  scope :upcoming, -> {where('due_on >= ?', Date.current)}
+  scope :past, -> {where('due_on < ?', Date.current)}
+
+  def status
+    return "Pending" unless self.completed
+    "Completed"
+  end
 end
